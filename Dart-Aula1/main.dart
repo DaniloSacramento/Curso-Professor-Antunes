1a9782afaf67b927f0c6c43121137dcf277713c4
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-10-04 14:28:33 -0300

    começando o curso

---
+++ Dart-Aula1/main.dart
@@ -0,0 +1,11 @@
+main(){
+  int ano = 2002;
+  String nome = "Danilo";
+  double real = 9.7;
+
+  print("Estamos no ano $ano e meu amigo se chama $nome");
+
+  if(nome == "Danilo"){
+    print("Ola Danilo");
+  }
+}
